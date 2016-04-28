f7faf431d764b3c4a43f65dacb7474f50bc2544b
Sancho <Sanchosmit@gmail.com>
2016-04-28 23:18:34 +0300

    Added new weather parameters and rates features

--- bot.py
+++ bot.py
@@ -3,28 +3,38 @@ import telebot
 import exchange_rates as rt
 import weather as wth
 
-
 bot = telebot.TeleBot(config.token)
+
+
+def command_split(string):
+    return string.split()
 
 
 @bot.message_handler(commands=['start', 'help'])
 def handle_start_help(message):
-    str = "Приветствую, Вас, хозяин!" + "\n"\
-          + "Я умею выполнять такие команды: " + "\n"\
-          + "/rates - курс валют" + "\n"\
-          + "/weather - текущая температура и влажность" + "\n"\
+    str = "Приветствую, Вас, хозяин!" + "\n" \
+          + "Я умею выполнять такие команды: " + "\n" \
+          + "/rates - курс валют" + "\n" \
+          + "Параметры для /rates: " + "\n" \
+          + "USD - доллар США" + "\n" \
+          + "EUR - евро" + "\n" \
+          + "RUR - рубль" + "\n" \
+          + "/weather_current - текущая погода" + "\n" \
           + "/help - справка"
     bot.send_message(message.chat.id, str)
 
 
 @bot.message_handler(commands=['rates'])
 def handle_rates(message):
-    bot.send_message(message.chat.id, rt.get_rates("USD"))
-    bot.send_message(message.chat.id, rt.get_rates("EUR"))
-    bot.send_message(message.chat.id, rt.get_rates("RUR"))
+    if len(command_split(message.text)) > 1:
+        bot.send_message(message.chat.id, rt.get_rates(command_split(message.text)[1]))
+    else:
+        bot.send_message(message.chat.id, rt.get_rates("USD"))
+        bot.send_message(message.chat.id, rt.get_rates("EUR"))
+        bot.send_message(message.chat.id, rt.get_rates("RUR"))
 
 
-@bot.message_handler(commands=['weather'])
+@bot.message_handler(commands=['weather_current'])
 def handle_current_weather(message):
     bot.send_message(message.chat.id, wth.get_current_weather())
 
--- weather.py
+++ weather.py
@@ -2,6 +2,7 @@ import json
 import requests
 
 
+
 def get_current_weather():
     # API url
     url = "http://api.openweathermap.org/data/2.5/weather?q=Odessa,ua&units=metric&APPID=888f6def0b40b79bce8c0cf3ebd774d6"
@@ -10,6 +11,10 @@ def get_current_weather():
     # Parse JSON data
     data = json.loads(response.text)
     # Weather init
-    weather = 'Температура: ' + str(data['main']['temp']) + ' °C' + '\n' \
-              + 'Влажность: ' + str(data['main']['humidity']) + ' %' + '\n'
+    weather = 'Город: ' + data['name'] + '\n' \
+                + 'Температура: ' + str(data['main']['temp']) + ' °C' + '\n' \
+                + 'Влажность: ' + str(data['main']['humidity']) + ' %' + '\n'\
+                + 'Давление: ' + str(data['main']['pressure']) + ' кПа' + '\n'\
+                + 'Облачность: ' + str(data['clouds']['all']) + ' %' + '\n'\
+                + 'Скорость ветра: ' + str(data['wind']['speed']) + ' м/c' + '\n'
     return weather
